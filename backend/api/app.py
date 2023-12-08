0d01fdf75a7bab0c8e3847f571167128d8d4cfd0
Avenir <kadomcev2004@gmail.com>
2023-12-08 23:54:36 +0500

    Add email router

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,6 +3,7 @@ from .auth.routes import router as auth_router
 from backend.api.database import init_models, Base, engine
 from .chat.routes import router as chat_router
 from .documents.router import router as doc_router
+from .email.routers import router as email_router
 import asyncio
 
 def create_app() -> FastAPI:
@@ -16,6 +17,7 @@ def create_app() -> FastAPI:
     _app.include_router(chat_router)
     _app.include_router(auth_router)
     _app.include_router(doc_router)
+    _app.include_router(email_router)
 
     return _app
 
