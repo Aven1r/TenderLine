b036670c9edc8a043a1860ac286c839036d9d6f6
Avenir <kadomcev2004@gmail.com>
2023-12-09 04:25:31 +0500

    Fix router issue. Add docker frontend build

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.staticfiles import StaticFiles
 from .auth.routes import router as auth_router
-from backend.api.database import init_models, Base, engine
 from .chat.routes import router as chat_router
 from .documents.router import router as doc_router
+from .database import init_models
+from .web_endpoints import router as web_router
 from .email.routers import router as email_router
-import asyncio
 
 
 def create_app() -> FastAPI:
@@ -15,6 +16,9 @@ def create_app() -> FastAPI:
     # Base.metadata.create_all(engine)
 
     _app = FastAPI()
+    _app.mount('/static', StaticFiles(directory='frontend/static'), name='static')
+
+    _app.include_router(web_router)
     _app.include_router(chat_router)
     _app.include_router(auth_router)
     _app.include_router(doc_router)
@@ -28,5 +32,5 @@ app = create_app()
 
 @app.on_event("startup")
 async def on_startup():
-    await init_models()
+    # await init_models()
     pass
